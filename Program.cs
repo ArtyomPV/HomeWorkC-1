26d353f248477bb03e6b64e779cb3c11db20556a
Artyom <itcoding76@gmail.com>
2022-11-30 08:59:32 +0200

    task#2  is solved

--- Program.cs
+++ Program.cs
@@ -3,7 +3,16 @@
 принимает два числа и выдает, какое число большее, 
 а какое меньшее
 */
+Console.Clear();
 
+Console.Write("Input first number: ");
+int number1  = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input second number: ");
+int number2  = Convert.ToInt32(Console.ReadLine());
+
+if(number1>number2) Console.Write("first number is maximus, second number is minimal.");
+else Console.Write("first number is minimal, second number is maximus.");
 /*
 Задача 4. Напишите программк, которая принимает
 на вход три числа и выдает максимальное из этих чисел
