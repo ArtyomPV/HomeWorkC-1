ef0aed459b70bece9a1a9f0318cce0e81458b2bf
Artyom <itcoding76@gmail.com>
2022-11-30 09:28:03 +0200

    task#4  is solved

--- Program.cs
+++ Program.cs
@@ -3,7 +3,8 @@
 принимает два числа и выдает, какое число большее, 
 а какое меньшее
 */
-Console.Clear();
+//===============task 2========================
+/*Console.Clear();
 
 Console.Write("Input first number: ");
 int number1  = Convert.ToInt32(Console.ReadLine());
@@ -13,16 +14,38 @@ int number2  = Convert.ToInt32(Console.ReadLine());
 
 if(number1>number2) Console.Write("first number is maximus, second number is minimal.");
 else Console.Write("first number is minimal, second number is maximus.");
+*/
+
 /*
-Задача 4. Напишите программк, которая принимает
+Задача 4. Напишите программу, которая принимает
 на вход три числа и выдает максимальное из этих чисел
 */
+//===============task 4========================
+Console.Clear();
+
+Console.Write("Enter first number: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter second number: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter third number: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+int maxNumber = number1;
+if(number2 > maxNumber) maxNumber = number2;
+if(number3 > maxNumber) maxNumber = number3;
+
+Console.Write("\n");
+Console.WriteLine($"The entered number {maxNumber}" + " is maximum \n");
+
 
 /*
 Задача 6. Напишите программу, которая на вход принимает
 число и выдает, является ли данное число четным
 (делится без остатка).
 */
+
 
 /*
 Задача 6. Напишите программу, которая на вход принимает 
