72d183c5845b46773b4326fbe3b034f06a653ebb
Artyom <itcoding76@gmail.com>
2022-12-01 11:28:43 +0200

    Task#8 is solved removed  condition if(count%2==0)

--- Program.cs
+++ Program.cs
@@ -77,9 +77,9 @@ while(number <=0) {
 int count = 2;
 
 while (count<=number){
-    if (count%2==0){
+    
         if(count<number-1)   Console.Write($"{count}, ");
         else Console.Write(count);
-    }
+ 
     count+=2;
 }
